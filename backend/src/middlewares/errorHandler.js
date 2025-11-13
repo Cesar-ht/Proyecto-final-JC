65ccc6cba0ec620457a7828fcdd40d9332a79fb7
Cesar-ht <cesarenriqueholguin1@gmail.com>
2025-11-13 16:17:36 -0500

    Se actualiza el package.json del backend y se actualiza el middleware

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,7 +1,11 @@
-export default function errorHandler(err, req, res, next) {
-  console.error(err.stack)
+const errorHandler = (err, req, res, next) => {
+  console.error('Error:', err.stack)
+  
   res.status(err.status || 500).json({
+    success: false,
     error: true,
     message: err.message || 'Error interno del servidor'
   })
 }
+
+export default errorHandler
